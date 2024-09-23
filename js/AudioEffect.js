58d9776f91f295876328f9c65fae85bf55e34866
BearHubs <bear90789078@gmail.com>
2024-09-23 16:40:39 +0800

    Update AudioEffect.js

--- js/AudioEffect.js
+++ js/AudioEffect.js
@@ -3,6 +3,7 @@ export class AudioEffect {
         this.backgroundMusic = [];
         this.currentTrackIndex = 0;
         this.soundEffects = {};
+        this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
     }
 
     loadBackgroundMusic(urls) {
@@ -15,22 +16,18 @@ export class AudioEffect {
     }
 
     playBackgroundMusic() {
-        if (this.backgroundMusic.length > 0) {
-            this.backgroundMusic[this.currentTrackIndex].play();
-        }
+        this._playAudio(this.backgroundMusic[this.currentTrackIndex]);
     }
 
     pauseBackgroundMusic() {
-        if (this.backgroundMusic.length > 0) {
-            this.backgroundMusic[this.currentTrackIndex].pause();
-        }
+        this._pauseAudio(this.backgroundMusic[this.currentTrackIndex]);
     }
 
     playNextTrack() {
-        this.backgroundMusic[this.currentTrackIndex].pause();
+        this._pauseAudio(this.backgroundMusic[this.currentTrackIndex]);
         this.currentTrackIndex = (this.currentTrackIndex + 1) % this.backgroundMusic.length;
         this.backgroundMusic[this.currentTrackIndex].currentTime = 0;
-        this.backgroundMusic[this.currentTrackIndex].play();
+        this._playAudio(this.backgroundMusic[this.currentTrackIndex]);
     }
 
     loadSoundEffect(name, url) {
@@ -41,7 +38,68 @@ export class AudioEffect {
     playSoundEffect(name) {
         if (this.soundEffects[name]) {
             this.soundEffects[name].currentTime = 0;
-            this.soundEffects[name].play();
+            this._playAudio(this.soundEffects[name]);
+        }
+    }
+
+    _playAudio(audio) {
+        if (audio) {
+            audio.play().catch(error => {
+                console.warn('Audio playback failed:', error);
+            });
+        }
+    }
+
+    _pauseAudio(audio) {
+        if (audio) {
+            audio.pause();
         }
     }
 }
+// export class AudioEffect {
+//     constructor() {
+//         this.backgroundMusic = [];
+//         this.currentTrackIndex = 0;
+//         this.soundEffects = {};
+//     }
+
+//     loadBackgroundMusic(urls) {
+//         this.backgroundMusic = urls.map(url => {
+//             const audio = new Audio(url);
+//             audio.loop = false;
+//             audio.addEventListener('ended', () => this.playNextTrack());
+//             return audio;
+//         });
+//     }
+
+//     playBackgroundMusic() {
+//         if (this.backgroundMusic.length > 0) {
+//             this.backgroundMusic[this.currentTrackIndex].play();
+//         }
+//     }
+
+//     pauseBackgroundMusic() {
+//         if (this.backgroundMusic.length > 0) {
+//             this.backgroundMusic[this.currentTrackIndex].pause();
+//         }
+//     }
+
+//     playNextTrack() {
+//         this.backgroundMusic[this.currentTrackIndex].pause();
+//         this.currentTrackIndex = (this.currentTrackIndex + 1) % this.backgroundMusic.length;
+//         this.backgroundMusic[this.currentTrackIndex].currentTime = 0;
+//         this.backgroundMusic[this.currentTrackIndex].play();
+//     }
+
+//     loadSoundEffect(name, url) {
+//         this.soundEffects[name] = new Audio(url);
+//         this.soundEffects[name].load();
+//     }
+
+//     playSoundEffect(name) {
+//         if (this.soundEffects[name]) {
+//             this.soundEffects[name].currentTime = 0;
+//             this.soundEffects[name].play();
+//         }
+//     }
+// }
